822b51b84f16ca2f73b4bd3422ef0e49cf97734a
Malvin Valerian <malvinval09@gmail.com>
2023-07-18 19:11:39 +0700

    add navbar and hero section

---
+++ Detikcom_FrontendDesignAssignment_MalvinValerianGultom/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,}"],
+  theme: {
+    extend: {
+      colors: {
+        "detik-blue": "#007CBD"
+      },
+      fontFamily: {
+        "poppins": ['Poppins', 'sans-serif'],
+        "montserrat": ['Montserrat', 'sans-serif'],
+        "manrope": ['Manrope', 'sans-serif']
+      },
+      fontWeight: {
+        "bold-400": "400",
+        "bold-900": "900",
+        "bold-600": "600"
+      },
+      borderRadius: {
+        "half": "50px"
+      },
+      backgroundImage: {
+        "detik-hero-image": "url('./img/hero-image.png')"
+      },
+      margin: {
+        "detik-hero-6rem": "2rem"
+      }
+    },
+  },
+  plugins: [],
+}
+
